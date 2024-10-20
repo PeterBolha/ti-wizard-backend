51208004bc3cd21c9304bc08698ff22f65b81c7b
alexarlord-boop <rezerw0.1@yandex.ru>
2024-10-20 11:05:15 +0300

    Refactored structure: dirs, modules naming, settings

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tiwizardbackend project.
+URL configuration for server project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("metadata_importer/", include("metadata_importer.urls")),
+    # path("metadata_importer/", include("metadata_importer.urls")),
 ]

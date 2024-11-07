966f2162f10e48dcd0c21df753aa142448bb231b
alexarlord-boop <rezerw0.1@yandex.ru>
2024-11-07 10:07:55 +0200

    Updated cors settings

--- server/config/settings/settings.py
+++ server/config/settings/settings.py
@@ -94,21 +94,21 @@ MIDDLEWARE = [
 #     "http://localhost:8000",
 #     "http://127.0.0.1:8000",
 # ]
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:8080',  # Your React app's origin, to be updated (?)
-#     "http://localhost:8000",
-#     "http://127.0.0.1:8000",
-# ]
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080',  # Your React app's origin, to be updated (?)
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
 #
 # CORS_ALLOW_CREDENTIALS = True  # Allow credentials (sessions)
-#
-# CORS_ALLOW_METHODS = [
-#     'GET',
-#     'POST',
-#     'PATCH',
-#     'DELETE',
-#     'OPTIONS',
-# ]
+
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PATCH',
+    'DELETE',
+    'OPTIONS',
+]
 
 ##################################################################
 # CSRF settings

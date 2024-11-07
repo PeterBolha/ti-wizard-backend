10706cab55115d0537c1afc9c9b2e997f6af9c9f
alexarlord-boop <rezerw0.1@yandex.ru>
2024-11-07 09:50:34 +0200

    Updated user auto generation -- creds passed via environment variables defined in the compose.yml

--- server/create_superuser.py
+++ server/create_superuser.py
@@ -1,5 +1,3 @@
-# server/create_superuser.py
-import argparse
 import logging
 import os
 
@@ -7,25 +5,17 @@ import django
 from django.contrib.auth import get_user_model
 from django.utils.crypto import get_random_string
 
-# Set up logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description='Create a Django superuser.')
-parser.add_argument('--username', type=str, help='Username for the superuser')
-parser.add_argument('--email', type=str, help='Email for the superuser')
-parser.add_argument('--password', type=str, help='Password for the superuser')
-args = parser.parse_args()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 User = get_user_model()
 
-username = args.username if args.username else "user_" + get_random_string(8)
-password = args.password if args.password else get_random_string(12)
-email = args.email if args.email else f"{username}@example.com"
+username = os.getenv('SUPERUSER_USERNAME', 'user_' + get_random_string(8))
+email = os.getenv('SUPERUSER_EMAIL', 'default@example.com')
+password = os.getenv('SUPERUSER_PASSWORD', get_random_string(12))
 
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
